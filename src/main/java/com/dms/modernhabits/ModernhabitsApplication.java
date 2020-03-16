b3841705f5b9d1b587d8ed8ed5c1e98bb0740f5f
David Martínez <davidmartinezsepulveda@gmail.com>
2020-03-16 16:32:24 +0100

    first commit

---
+++ src/main/java/com/dms/modernhabits/ModernhabitsApplication.java
@@ -0,0 +1,13 @@
+package com.dms.modernhabits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernhabitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernhabitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dms/modernhabits/ModernhabitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dms.modernhabits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernhabitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
