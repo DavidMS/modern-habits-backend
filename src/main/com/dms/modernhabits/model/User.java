936a40d354c16378db0ee78894d948e0b2b94b48
David Martínez[color] <davidmartinezsepulveda@gmail.com>
2020-04-04 19:50:59 +0200

    finally fixed everything

--- src/main/com/dms/modernhabits/model/User.java
+++ src/main/com/dms/modernhabits/model/User.java
@@ -11,7 +11,7 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    
+
     private String name;
 
     @OneToOne(fetch = FetchType.LAZY,
